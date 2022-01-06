fd4f1db1e55cbeb60de957c8390dfdbea60909a6
Veronika <verona.binko@gmail.com>
2022-01-06 10:48:39 +0300

    Feat: add API route with mongoDB

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,10 +1,21 @@
 //POST /api/new-meetup
+import { MongoClient} from 'mongodb';
 
-function handler(req, res) {
+async function handler(req, res) {
   if (req.method === 'POST') {
     const data = req.body;
 
-    const { title, image, adress, description } = data;
+    const client = await MongoClient.connect('mongodb+srv://veronaten:73330442929@cluster0.p9t2g.mongodb.net/meetups?retryWrites=true&w=majority');
+
+    const db = client.db();
+
+    const meetupsCollection = db.collection('meetups');
+    const result = await meetupsCollection.insertOne(data);
+    console.log(result);
+
+    client.close();
+
+    res.status('201').json({ message: 'Meetup insert!' });
   }
 };
 
